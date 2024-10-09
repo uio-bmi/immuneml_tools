1bc13ddcffab95a0fcd6f512e8456bec8d6dc0fe
Lonneke Scheffer <lonsch96@live.nl>
2024-10-08 22:44:06 -0700

    tool for simulating random dataset with button-based interface

--- build_dataset_yaml_wrapper.py
+++ build_dataset_yaml_wrapper.py
@@ -1,4 +1,5 @@
 import sys
 from immuneML.api.galaxy.build_dataset_yaml import main
+
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ build_dummy_data_simulation_wrapper.py
@@ -0,0 +1,5 @@
+import sys
+from immuneML.api.galaxy.build_dummy_data_simulation_yaml import main
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
